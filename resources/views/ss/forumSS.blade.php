338dc7814c99309f900986ad994934f9170503e6
fmgaming180118 <fachrifahira15@gmail.com>
2025-01-23 09:13:00 +0700

    dropdown menu SS

--- resources/views/ss/forumSS.blade.php
+++ resources/views/ss/forumSS.blade.php
@@ -344,6 +344,20 @@
             }
         }
     </style>
+
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script src="{{ asset('assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
+<script>
+    $(document).ready(function() {
+        // Hover function for dropdowns
+        $('.nav-item.dropdown').hover(function() {
+            $(this).find('.dropdown-menu').first().stop(true, true).slideDown(150);
+        }, function() {
+            $(this).find('.dropdown-menu').first().stop(true, true).slideUp(150);
+        });
+    });
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
