19cefcad63ffba55b3c86329458afb3e30e6fbd6
fmgaming180118 <fachrifahira15@gmail.com>
2025-01-23 09:50:13 +0700

    dropdown menu productions

--- resources/views/maintenance/index.blade.php
+++ resources/views/maintenance/index.blade.php
@@ -97,6 +97,20 @@
             </div>
         </section>
 
+                        <!-- jQuery -->
+                        <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+                        <script src="{{ asset('assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
+                        <script>
+                            $(document).ready(function() {
+                                // Hover function for dropdowns
+                                $('.nav-item.dropdown').hover(function() {
+                                    $(this).find('.dropdown-menu').first().stop(true, true).slideDown(150);
+                                }, function() {
+                                    $(this).find('.dropdown-menu').first().stop(true, true).slideUp(150);
+                                });
+                            });
+                            </script>
+
 
     </main><!-- End #main -->
 @endsection
